d3239e3bd9a9f032d454a70bbb64be6dd4bbc9f6
juddata <127310607+juddata@users.noreply.github.com>
2023-12-20 14:51:57 +0000

    Add files via upload
    My first codes in python

---
+++ My_first_code.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This is my first line of code
+
+
+# In[2]:
+
+
+print("My name is Judit Zalai")
+print("The date is December 20th")
+
+
+# In[3]:
+
+
+# Write a program to analyse real estate data.
+
+
+# In[14]:
+
+
+# Remember Python does not use a comma to separate thousands
+# Create variables
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+# Add all the sales together 
+total_sales = a + b + c + d + e
+
+# View the outputprint(total_sales)
+print(total_sales)
+
+
+# In[15]:
+
+
+avg_sales= total_sales/5
+print(avg_sales)
+
+
+# In[18]:
+
+
+# create a new variable
+# Calculate the price of property C as sales - commission
+property_price_c = (67000 - 6500)
+
+# View the output
+print(property_price_c)
+
